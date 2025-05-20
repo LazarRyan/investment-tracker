c19633c5ee6a1d200dd19cc66b2ed82c2608751b
Ryan Lazar <Lazar.ryan123@gmail.com>
2025-05-20 19:14:44 -0400

    Fix: Add sector property to InvestmentWithMarketData interface

--- app/components/InvestmentPortfolio.tsx
+++ app/components/InvestmentPortfolio.tsx
@@ -13,6 +13,7 @@ interface InvestmentWithMarketData extends Investment {
   totalValue?: number;
   gainLoss?: number;
   gainLossPercentage?: number;
+  sector?: string;
 }
 
 interface InvestmentPortfolioProps {
