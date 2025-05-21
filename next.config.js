be0229aa69e0591452dfbd24fcb8b4a37b49797b
Ryan Lazar <Lazar.ryan123@gmail.com>
2025-05-20 20:03:58 -0400

    chore: optimize deployment configuration for Vercel

--- next.config.js
+++ next.config.js
@@ -3,10 +3,23 @@ const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 
 const nextConfig = {
   reactStrictMode: true,
+  output: 'standalone',
+  experimental: {
+    serverMinification: true,
+    serverSourceMaps: false,
+  },
   webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
     // Add case sensitivity plugin
     config.plugins = config.plugins || [];
     config.plugins.push(new CaseSensitivePathsPlugin());
+
+    // Optimize the build
+    if (isServer) {
+      config.optimization = {
+        ...config.optimization,
+        minimize: true,
+      };
+    }
 
     return config;
   },
